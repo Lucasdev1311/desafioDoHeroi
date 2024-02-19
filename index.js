4d8405e252b08d49bbd3accc34729e36a55e825a
Lucasdev1311 <lucasdev1311@gmail.com>
2024-02-19 16:36:28 -0300

    Meu primeiro projeto em JavaScript

---
+++ index.js
@@ -0,0 +1,30 @@
+let nome = "Pilgs"
+let pontosDoHeroi = 5599
+let i = pontosDoHeroi
+
+for(  i ; i <= 7000 ; i+=13){
+  
+ console.log(i)
+ console.log("subiu de nivel ")
+}
+
+console.log("Pontos de experiência " + i)
+
+if(i < 1000){
+  console.log("o heroi " +nome+ " esta no nivel ferro")
+} else if( i <= 2000){
+  console.log("o heroi " +nome+  " esta no nivel bronze")
+} else if(i  <= 5000){
+  console.log("o heroi " +nome+ " esta no nivel prata")
+} else if(i  <= 7000){
+  console.log("o heroi " +nome+ " esta no nivel ouro")
+} else if(i <= 8000){
+  console.log("o heroi " +nome+ " esta no nivel platina")
+} else if(i <= 9000){
+  console.log("o heroi " +nome+ " esta no nivel ascendente")
+} else if(i <= 10000){
+  console.log("o heroi " +nome+ " esta no nivel imortal")
+} else if(i >= 10001){
+  console.log("o heroi " +nome+ " Esta Radiante")
+}
+
